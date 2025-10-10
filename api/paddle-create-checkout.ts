9d1c79720a30fbea5abc2fa141e8a165190be283
Abdulrahman <dhmychistore@gmail.com>
2025-10-10 06:25:20 +0300

    fix(paddle): remove Bearer prefix from Authorization header for apikey_ tokens

--- api/paddle-create-checkout.ts
+++ api/paddle-create-checkout.ts
@@ -66,7 +66,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
     const response = await axios.post(`${baseUrl}/transactions`, payload, {
       headers: {
-        Authorization: `Bearer ${paddleToken}`,
+        Authorization: paddleToken,
         'Content-Type': 'application/json',
         Accept: 'application/json',
       },
